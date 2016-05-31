58d1fead7be0bec7100df6c0a5bc252ae1aefc1b
Stephanie Miranda <stephanie.miranda.valdez@gmail.com>
2016-05-30 19:29:58 -0500

    added todos

--- client/main.js
+++ client/main.js
@@ -1,7 +1,10 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
+import { Tasks } from '../api/collection.js';
 
 import './main.html';
+
+
 
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
@@ -25,5 +28,28 @@ Template.hello.events({
 Template.stephanie.events({
   'click #steph-button'(event, instance) {
     console.log('button works?');
+  }
+});
+
+Template.body.helpers({
+  tasks(){
+   return Tasks.find({});
+  },
+});
+
+Template.body.events({
+  'submit .new-task'(event){
+    event.preventDefault();
+    console.log('submit works');
+
+    var input = $('#todo-input').val();
+    console.log(input);
+
+    Tasks.insert({
+      text: input,
+      createdAt: new Date()
+    });
+
+    $('#todo-input').val('');
   }
 });
--- server/main.js
+++ server/main.js
@@ -1,5 +1,7 @@
 import { Meteor } from 'meteor/meteor';
+import '../api/collection.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
+  console.log('the server is running');
 });
